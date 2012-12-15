2969eb43be88b74a6112d8ecd708eadb076c6335
Tom Nealon <tgnealon@gmail.com>
2012-12-15 23:10:02 +0000

    Setup

---
+++ src/tree_induction/Tree_induction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tree_induction;
+
+/**
+ *
+ * @author tom
+ */
+public class Tree_induction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
