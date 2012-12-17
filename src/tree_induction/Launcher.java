9d73a56fa06327c7df3e459cad8c28dc337d1bcc
Tom Nealon <tgnealon@gmail.com>
2012-12-17 15:46:22 +0000

    Add metadata methods to Reader.

--- src/tree_induction/Launcher.java
+++ src/tree_induction/Launcher.java
@@ -13,17 +13,20 @@ public class Launcher {
     
     public static void launch() throws IOException {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));      
-    	Reader myReader = new Reader("./data_sources/car.data");
-    	System.out.println(myReader.getCell(0, 3));
-    	System.out.println(myReader.getCell(4, 2));
-//        System.out.println(myReader.calcNoRows());
-//        System.out.println(myReader.calcNoCols());
+    	//Reader myReader = new Reader("./data_sources/car.data");
+        Reader myReader = new Reader("./data_sources/Book1.csv");
+//    	System.out.println(myReader.getCell(0, 3));
+//    	System.out.println(myReader.getCell(4, 2));
+//        System.out.println(myReader.noRows);
+//        System.out.println(myReader.noCols);
         ToStringHelper help = new ToStringHelper();
-        String classes = help.toString(myReader.getClasses(3));
+        String classes = help.toString(myReader.getValues(4));
         System.out.println(classes);
     	myReader.printRow(0);
-        treeSelection myTree = new treeSelection(myReader, 6);
-        
+        System.out.println();
+        treeSelection myTree = new treeSelection(myReader, 4);
+        double info = myTree.infoNeed(4);
+        System.out.println(info);
     }
     
     
--- src/tree_induction/Reader.java
+++ src/tree_induction/Reader.java
@@ -13,29 +13,39 @@ public class Reader {
         public int noCols;
     
 	private ArrayList<ArrayList> store = new ArrayList<>();
-        private ArrayList<Set> attClasses = new ArrayList<>();
+        private ArrayList<Set> attValues = new ArrayList<>();
 	private boolean setEmptyToNull = true;
         private boolean firstRowLabels = false;
         
         private int startRow = 0;
 
 	public Reader(String fileName) throws IOException {
-		BufferedReader CSVFile = new BufferedReader(new FileReader(fileName));
-		String dataRow = CSVFile.readLine();
-		while (dataRow != null) {
-			String[] dataArray = dataRow.split(",");
-			store.add(setTypeList(dataArray));
-		    dataRow = CSVFile.readLine();
-	    }
+            BufferedReader CSVFile = new BufferedReader(new FileReader(fileName));
+            String dataRow = CSVFile.readLine();
+            while (dataRow != null) {
+                String[] dataArray = dataRow.split(",");
+                store.add(setTypeList(dataArray));
+                dataRow = CSVFile.readLine();
+            }
             CSVFile.close();
             // Metadata calculations for this dataset.
             calcNoCols();
             calcNoRows();
-            buildAttClasses();
+            buildAttValues();
 	}
+       
+        public int valueOccurs(String value, int col) {
+            int occurances = 0;
+            for(int i = 0; i < noRows; i++) {
+                if(getCell(i, col).equals(value)) {
+                    occurances++;
+                }
+            }
+            return occurances;
+        }
         
-        public Set getClasses(int col) {
-            return attClasses.get(col);
+        public Set getValues(int col) {
+            return attValues.get(col);
         }
 	
 	public ArrayList getRow(int row) {
@@ -88,13 +98,13 @@ public class Reader {
             return noCols;
         }
         
-        private void buildAttClasses() {
+        private void buildAttValues() {
             for(int i = 0; i < noCols; i++) {
-                attClasses.add(getAttClasses(i));
+                attValues.add(getAttValues(i));
             }
         }
         
-        private Set<String> getAttClasses(int col) {
+        private Set<String> getAttValues(int col) {
             Set<String> colClasses = new HashSet<>();
             for(int i = startRow; i < noRows; i++) {
                 colClasses.add(getCell(i, col));
